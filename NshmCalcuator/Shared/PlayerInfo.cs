24745bbc09708d716da97abacf2bafee42914864
dellbeat <daizhicai@foxmail.com>
2023-11-15 16:20:01 +0800

    搭建脚手架

---
+++ NshmCalcuator/Shared/PlayerInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace NshmCalculator.Shared;
+
+public class PlayerInfo
+{
+    /// <summary>
+    /// 玩家基础攻击力
+    /// </summary>
+    public int PlayerBaseAttack { get; set; }
+    
+    /// <summary>
+    /// 新增攻击力
+    /// </summary>
+    public int IncreaseAttack { get; set; }
+}
