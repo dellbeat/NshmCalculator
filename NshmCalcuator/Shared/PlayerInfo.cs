f0d22fc7fb97565c21a6731d06e4f5058463f2a1
dellbeat <daizhicai@foxmail.com>
2023-11-16 01:50:20 +0800

    完善了实体类的注释定义，对主界面进行了排版

--- NshmCalcuator/Shared/PlayerInfo.cs
+++ NshmCalcuator/Shared/PlayerInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace NshmCalculator.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NshmCalculator.Shared;
 
 public class PlayerInfo
 {
@@ -7,42 +9,54 @@ public class PlayerInfo
     /// <summary>
     /// 玩家基础攻击力
     /// </summary>
+    [Required]
+    [Range(1,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int PlayerBaseAttack { get; set; }
 
     /// <summary>
     /// 玩家基础克制
     /// </summary>
+    [Required]
+    [Range(1,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int PlayerBaseRestraint { get; set; }
-    
+
     /// <summary>
     /// 玩家基础属性（元素）攻击
     /// </summary>
+    [Required]
+    [Range(1,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int PlayerBaseElementAttack { get; set; }
-    
+
     /// <summary>
     /// 玩家基础破防
     /// </summary>
+    [Required]
+    [Range(1,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int PlayerBaseBreakDefense { get; set; }
-    
+
     /// <summary>
     /// 玩家基础命中
     /// </summary>
+    [Required]
+    [Range(1,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int PlayerBaseHit { get; set; }
-    
+
     /// <summary>
     /// 玩家基础会心
     /// </summary>
+    [Required]
+    [Range(1,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int PlayerBaseCriticalHits { get; set; }
-    
+
     /// <summary>
     /// 玩家基础会伤率
     /// </summary>
-    public double PlayerBaseCriticalRate { get; set; }
-    
+    public int PlayerBaseCriticalRate { get; set; }
+
     /// <summary>
     /// 玩家周天会心率
     /// </summary>
-    public double PlayerBaseZtCriticalHitsRate { get; set; }
+    public int PlayerBaseZtCriticalHitsRate { get; set; }
 
     #endregion
 
@@ -52,36 +66,36 @@ public class PlayerInfo
     /// 新增攻击力
     /// </summary>
     public int IncreaseAttack { get; set; }
-    
+
     /// <summary>
     /// 新增克制
     /// </summary>
-    public int IncreaseBaseRestraint { get; set; }
-    
+    public int IncreaseRestraint { get; set; }
+
     /// <summary>
     /// 新增属性攻击
     /// </summary>
     public int IncreaseElementAttack { get; set; }
-    
+
     /// <summary>
     /// 新增破防
     /// </summary>
     public int IncreaseBreakDefense { get; set; }
-    
+
     /// <summary>
     /// 新增命中
     /// </summary>
     public int IncreaseHit { get; set; }
-    
+
     /// <summary>
     /// 新增会心
     /// </summary>
     public int IncreaseCriticalHits { get; set; }
-    
+
     /// <summary>
     /// 新增会伤率
     /// </summary>
-    public double IncreaseCriticalRate { get; set; }
+    public int IncreaseCriticalRate { get; set; }
 
     #endregion
 
@@ -90,17 +104,35 @@ public class PlayerInfo
     /// <summary>
     /// 敌方防御
     /// </summary>
-    public double EnemyDefense { get; set; }
-    
+    public int EnemyDefense { get; set; }
+
     /// <summary>
     /// 敌方格挡
     /// </summary>
     public int EnemyBlock { get; set; }
-    
+
     /// <summary>
     /// 满命中
     /// </summary>
-    public int FullHit { get; set; }
+    public int FullHit => Convert.ToInt32(Math.Floor((105524 * EnemyBlock + 2541845) * 1.0 / (98394 - 5 * EnemyBlock)));
 
     #endregion
+    
+    /// <summary>
+    /// 结果（百分比增伤）
+    /// </summary>
+    public double Result { get; set; }
+
+    public PlayerInfo()
+    {
+        PlayerBaseAttack = 2500;
+        PlayerBaseRestraint = 1000;
+        PlayerBaseElementAttack = 1000;
+        PlayerBaseBreakDefense = 1000;
+        PlayerBaseHit = 500;
+        PlayerBaseCriticalHits = 888;
+        PlayerBaseCriticalRate = 188;
+        EnemyBlock = 630;
+        EnemyDefense = 1680;
+    }
 }
