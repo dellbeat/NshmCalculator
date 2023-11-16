c09459dbbf969ae7369154e15a5bedd1cb1813f7
dellbeat <daizhicai@foxmail.com>
2023-11-16 12:20:15 +0800

    完成计算器逻辑的编写及验证

--- NshmCalcuator/Shared/PlayerInfo.cs
+++ NshmCalcuator/Shared/PlayerInfo.cs
@@ -51,12 +51,16 @@ public class PlayerInfo
     /// <summary>
     /// 玩家基础会伤率
     /// </summary>
-    public int PlayerBaseCriticalRate { get; set; }
+    [Required]
+    [Range(0,1000,ErrorMessage = "请保证会伤百分比在0.0-1000.0范围内")]
+    public double PlayerBaseCriticalRate { get; set; }
 
     /// <summary>
     /// 玩家周天会心率
     /// </summary>
-    public int PlayerBaseZtCriticalHitsRate { get; set; }
+    [Required]
+    [Range(0,5,ErrorMessage = "请保证周天会心百分比在0.0-5.0范围内")]
+    public double PlayerBaseZtCriticalHitsRate { get; set; }
 
     #endregion
 
@@ -65,37 +69,51 @@ public class PlayerInfo
     /// <summary>
     /// 新增攻击力
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int IncreaseAttack { get; set; }
 
     /// <summary>
     /// 新增克制
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int IncreaseRestraint { get; set; }
 
     /// <summary>
     /// 新增属性攻击
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int IncreaseElementAttack { get; set; }
 
     /// <summary>
     /// 新增破防
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int IncreaseBreakDefense { get; set; }
 
     /// <summary>
     /// 新增命中
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int IncreaseHit { get; set; }
 
     /// <summary>
     /// 新增会心
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int IncreaseCriticalHits { get; set; }
 
     /// <summary>
     /// 新增会伤率
     /// </summary>
-    public int IncreaseCriticalRate { get; set; }
+    [Required]
+    [Range(0,10,ErrorMessage = "请保证新增会伤百分比在0.0-10.0范围内")]
+    public double IncreaseCriticalRate { get; set; }
 
     #endregion
 
@@ -104,11 +122,15 @@ public class PlayerInfo
     /// <summary>
     /// 敌方防御
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int EnemyDefense { get; set; }
 
     /// <summary>
     /// 敌方格挡
     /// </summary>
+    [Required]
+    [Range(0,100000,ErrorMessage = "请输入1至100000内的整数")]
     public int EnemyBlock { get; set; }
 
     /// <summary>
