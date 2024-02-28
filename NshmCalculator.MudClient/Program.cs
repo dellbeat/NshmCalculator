e7055f0a62fd079bedbf59db6b2df7e547d397f3
dellbeat <daizhicai@foxmail.com>
2024-02-28 22:10:44 +0800

    移植了素问治疗量计算器，调整了部分内容

--- NshmCalculator.MudClient/Program.cs
+++ NshmCalculator.MudClient/Program.cs
@@ -1,5 +1,7 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 using NshmCalculator.MudClient;
 
@@ -8,6 +10,19 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = true;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 300;
+    config.SnackbarConfiguration.ShowTransitionDuration = 300;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
+    
---
+++ NshmCalculator.MudClient/Utilities/TipsText.cs
@@ -0,0 +1,12 @@
+﻿namespace NshmCalculator.MudClient.Utilities;
+
+public static class TipsText
+{
+    public const string ExceptionText = "<ul><li>计算进程出现异常，建议检查输入内容</li><li>如有必要可与作者联系</li></ul>";
+
+    public const string FooterText = "Version:0.2.0 | Powered by .NET 8.0";
+
+    public const string LastTextName = "lastVisit";//最近一次访问的计算器路由
+
+    public const string AutoRedirectName = "autoRedirect";//是否重定向
+}
