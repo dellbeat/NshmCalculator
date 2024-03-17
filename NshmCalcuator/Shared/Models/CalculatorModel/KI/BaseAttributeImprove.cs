ec1070a6719555cb6d6c0bfe262f7fa4d5e0bc4e
dellbeat <daizhicai@foxmail.com>
2024-03-17 19:34:48 +0800

    补充必要文件，不提升版本（rider的提交用起来需要适应）

---
+++ NshmCalcuator/Shared/Models/CalculatorModel/KI/BaseAttributeImprove.cs
@@ -0,0 +1,27 @@
+﻿namespace NshmCalculator.Shared.Models.CalculatorModel.KI;
+
+/// <summary>
+/// 最大基础词条加成参考
+/// </summary>
+public class BaseAttributeImprove
+{
+    /// <summary>
+    /// 词条
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// 最大值
+    /// </summary>
+    public int MaxValue { get; set; }
+    
+    /// <summary>
+    /// 攻击分
+    /// </summary>
+    public double AttackScore { get; set; }
+    
+    /// <summary>
+    /// 提升率
+    /// </summary>
+    public double ImprovePercent { get; set; }
+}
