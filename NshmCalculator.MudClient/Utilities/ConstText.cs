ca3458f4f12105e397d938194a46fe7260ac31d9
dellbeat <daizhicai@foxmail.com>
2024-03-26 02:15:28 +0800

    追加提升页面版本号

--- NshmCalculator.MudClient/Utilities/ConstText.cs
+++ NshmCalculator.MudClient/Utilities/ConstText.cs
@@ -10,7 +10,7 @@ public static class ConstText
 
     public const string LimitText = "<ul><li>计算完成</li><li>本次破防/命中出现超限情况</li><li>已忽略超限部分的提升</li></ul>";
 
-    public const string FooterText = "Version:1.0.0 | Powered by .NET 8.0";
+    public const string FooterText = "Version:1.0.1 | Powered by .NET 8.0";
 
     public const string LastVisitName = "lastVisit";//最近一次访问的计算器路由
 
