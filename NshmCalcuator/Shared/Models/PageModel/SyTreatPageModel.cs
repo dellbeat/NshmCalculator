5ebf3db86758df18c9b86701aff85b3fd2106cb3
dellbeat <daizhicai@foxmail.com>
2024-11-11 01:14:33 +0800

    修复：紧急修复新旧缓存兼容性问题

--- NshmCalcuator/Shared/Models/PageModel/SyTreatPageModel.cs
+++ NshmCalcuator/Shared/Models/PageModel/SyTreatPageModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using NshmCalculator.Shared.Models.CalculatorModel;
 
 namespace NshmCalculator.Shared.Models.PageModel;
 
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
 public class SyTreatPageModel
 {
     [ValidateComplexType]
