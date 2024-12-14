4294e5824b81174cc7afef390de9840c980a5e14
dellbeat <daizhicai@foxmail.com>
2024-12-14 13:04:25 +0800

    调整PVE计算器主界面的渲染逻辑和配置文件的数据结构，修复子组件中一些影响使用的问题

--- NshmCalcuator/Shared/Models/CalculatorModel/CalculatorConfig/PveConfig.cs
+++ NshmCalcuator/Shared/Models/CalculatorModel/CalculatorConfig/PveConfig.cs
@@ -1,44 +1,25 @@
-﻿namespace NshmCalculator.Shared.Models.CalculatorModel.CalculatorConfig;
+﻿using NshmCalculator.Shared.Models.Interface;
 
-public class PveConfig
+namespace NshmCalculator.Shared.Models.CalculatorModel.CalculatorConfig;
+
+public class PveConfig: IGameConfig
 {
 	/// <summary>
 	/// 页面中所使用的公式
 	/// </summary>
 	public PveFormula[] PveFormulas { get; set; }
     
-	/// <summary>
-	/// 角色基础数值项
-	/// </summary>
-	public FrontParamInfo[] BaseParam { get; set; }
+    /// <summary>
+    /// 组别列表，确定界面渲染顺序
+    /// </summary>
+    public string[] CategoryArray { get; set; }
     
-	/// <summary>
-	/// 开关设置项
-	/// </summary>
-	public FrontParamInfo[] SwitchParam { get; set; }
+    /// <summary>
+    /// 前端选项
+    /// </summary>
+    public FrontParamInfo[] FrontParamInfoArray { get; set; }
     
-	/// <summary>
-	/// 增益类选项
-	/// </summary>
-	public FrontParamInfo[] BuffParam { get; set; }
+    public long InternalVersion { get; set; } = -99999;
     
-	/// <summary>
-	/// 首领数值项
-	/// </summary>
-	public FrontParamInfo[] BossParam { get; set; }
-    
-	/// <summary>
-	/// 伤害占比项
-	/// </summary>
-	public FrontParamInfo[] DamageParam { get; set; }
-    
-	/// <summary>
-	/// 覆盖率
-	/// </summary>
-	public FrontParamInfo[] CoverageParam { get; set; }
-    
-	/// <summary>
-	/// 当前加载的数据版本
-	/// </summary>
-	public long CurrentInternalVersion { get; set; } = -99999;
+    public string HelperText { get; set; }
 }
