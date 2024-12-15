e0f752d35ac397f69a4752980609f62c96b8a1d7
dellbeat <daizhicai@foxmail.com>
2024-12-15 23:14:21 +0800

    追加暂存

---
+++ NshmCalcuator/Shared/Models/CalculatorModel/Enums/ParamRuleMode.cs
@@ -0,0 +1,25 @@
+﻿namespace NshmCalculator.Shared.Models.CalculatorModel.Enums;
+
+public enum ParamRuleMode
+{
+    /// <summary>
+    /// 默认值，在规则检查中视为非法值
+    /// </summary>
+    Invalid,
+	/// <summary>
+	/// 通过指定前置代号并比较值加载对应的文本
+	/// </summary>
+	CompareOptions,
+    /// <summary>
+    /// 删除选项中与特定参数已选内容一致的选项
+    /// </summary>
+    RemoveSameOptions,
+    /// <summary>
+    /// 共享选项
+    /// </summary>
+    ShareOptions,
+    /// <summary>
+    /// 选择时自动给关联选项赋值，用于选择BOSS时更新相关数值
+    /// </summary>
+    RelatedData,
+}
