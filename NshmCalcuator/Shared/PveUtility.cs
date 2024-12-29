0b16dfcf2b27d2ac2f89f32fd5fae4973b92af5d
dellbeat <30553983+dellbeat@users.noreply.github.com>
2024-12-29 23:01:21 +0800

    调整计算类代码，去除不必要的表达式调用以优化实际性能表现

--- NshmCalcuator/Shared/PveUtility.cs
+++ NshmCalcuator/Shared/PveUtility.cs
@@ -7,9 +7,6 @@ namespace NshmCalculator.Shared;
 
 public class PveUtility
 {
-    private static readonly ExpressionContext ExpContext = new()
-        { Options = ExpressionOptions.StrictTypeMatching };
-
     /// <summary>
     /// 用于关联公式层级的映射
     /// </summary>
@@ -24,43 +21,26 @@ public class PveUtility
     /// 最终结果的代号与表达式实体映射
     /// </summary>
     private static readonly Dictionary<string, Expression> ResultFormulaExpressionDic = new();
-
+    
     /// <summary>
-    /// 用于关联参数代号与表达式实体的映射
+    /// 具有特殊规则的表达式集合
     /// </summary>
-    private static readonly Dictionary<string, Expression> ParamExpressionDic = new();
-
-    /// <summary>
-    /// 具有特殊规则的参数集合
-    /// </summary>
-    private static readonly Dictionary<string, SpecialParamRule> SpecialParamRuleDic = new();
-
     private static readonly Dictionary<string, SpecialFormulaRule> SpecialFormulaRuleDic = new();
+
+    /// <summary>
+    /// 初始化后包含的参数代号列表
+    /// </summary>
+    private static readonly List<string> ParamCodeList = new();
 
     public static void InitUtilityFromConfig(PveConfig config)
     {
-        ParamExpressionDic.Clear();
+        ParamCodeList.Clear();
         FormulaLevelDic.Clear();
         InternalFormulaExpressionDic.Clear();
         SpecialFormulaRuleDic.Clear();
-        InitParamDic(config.FrontParamInfoArray.Select(s => s.Code));
+        ParamCodeList.AddRange(config.FrontParamInfoArray.Select(s => s.Code));
         InitInternalFormulaDic(config.InternalFormulas);
         InitResultFormulaDic(config.ResultFormulas);
-    }
-
-    /// <summary>
-    /// 将参数初始化为表达式实体
-    /// </summary>
-    /// <param name="array"></param>
-    private static void InitParamDic(IEnumerable<string> array)
-    {
-        ParamExpressionDic.Clear();
-
-        foreach (var code in array)
-        {
-            Expression exp = new Expression("[value]");
-            ParamExpressionDic.Add(code, exp);
-        }
     }
 
     /// <summary>
@@ -71,16 +51,16 @@ public class PveUtility
     {
         foreach (var formula in array)
         {
-            Expression exp = new Expression(formula.Formula, ExpContext);
+            Expression exp = new Expression(formula.Formula, ExpressionOptions.StrictTypeMatching);
             foreach (string param in formula.FormulaParam)
             {
-                if (InternalFormulaExpressionDic.TryGetValue(param, out var formulaExp))
+                if (InternalFormulaExpressionDic.ContainsKey(param))
                 {
-                    exp.Parameters[param] = formulaExp;
+                    exp.Parameters[param] = string.Empty;
                 }
-                else if (ParamExpressionDic.TryGetValue(param, out var paramExp))
+                else if (ParamCodeList.Contains(param))
                 {
-                    exp.Parameters[param] = paramExp;
+                    exp.Parameters[param] = string.Empty;
                 }
             }
 
@@ -118,7 +98,7 @@ public class PveUtility
         var lambdaFormulas = array.Where(s => s.Rule != null && s.Rule.Any(y => y.Mode == FormulaMode.Lambda)).ToArray();
         foreach (var formula in lambdaFormulas)
         {
-            Expression exp = new Expression(formula.Formula, ExpContext);
+            Expression exp = new Expression(formula.Formula, ExpressionOptions.StrictTypeMatching);
             InternalFormulaExpressionDic.Add(formula.Code, exp);
             SpecialFormulaRuleDic.Add(formula.Code, formula.Rule.First(y => y.Mode == FormulaMode.Lambda));
         }
@@ -133,16 +113,16 @@ public class PveUtility
             {
                 Expression exp = InternalFormulaExpressionDic.TryGetValue(singleFormula.Code, out var relateExp)
                     ? relateExp
-                    : new Expression(singleFormula.Formula, ExpContext);
+                    : new Expression(singleFormula.Formula, ExpressionOptions.StrictTypeMatching);
                 foreach (string singleParam in singleFormula.FormulaParam)
                 {
                     if (InternalFormulaExpressionDic.TryGetValue(singleParam, out var formulaExp))
                     {
-                        exp.Parameters[singleParam] = formulaExp;
+                        exp.Parameters[singleParam] = string.Empty;
                     }
-                    else if (ParamExpressionDic.TryGetValue(singleParam, out var paramExp))
+                    else if (ParamCodeList.Contains(singleParam))
                     {
-                        exp.Parameters[singleParam] = paramExp;
+                        exp.Parameters[singleParam] = string.Empty;
                     }
                 }
 
@@ -177,53 +157,18 @@ public class PveUtility
     }
 
     /// <summary>
-    /// 针对内功公式提供的查找方法,需要挂载在<see cref="Expression">公式实例</see>>上
-    /// </summary>
-    /// <param name="itemValue">传入的内功名称</param>
-    /// <returns>如果能找到该内功名称，则返回内功级别，否则返回空值</returns>
-    private static string VLookup(string itemValue)
-    {
-        string result = string.Empty;
-
-        foreach ((string? code, var rule) in SpecialParamRuleDic)
-        {
-            if (rule.Mode == ParamRuleMode.ShareOptions && ParamExpressionDic[code].Evaluate().ToString() == itemValue)
-            {
-                result = ParamExpressionDic[rule.FrontParamCode].Evaluate().ToString();
-                break;
-            }
-        }
-
-        return result;
-    }
-
-    /// <summary>
-    /// 传入界面上所有选项的当前值，供后续计算用
-    /// </summary>
-    /// <param name="valueDic"></param>
-    public static void InsertParam(Dictionary<string, ParamValue> valueDic)
-    {
-        foreach ((string? code, var value) in valueDic)
-        {
-            if (ParamExpressionDic.TryGetValue(code, out var exp))
-            {
-                exp.Parameters["value"] = value.NumberMode ? value.NumberValue : value.StringValue;
-            }
-        }
-    }
-
-    /// <summary>
-    /// 针对结果公式调用计算方法（实际最后面是都要调用的）
+    /// 针对结果公式调用计算方法
     /// </summary>
     /// <param name="codeList"></param>
     /// <returns></returns>
-    public static List<double?> Calculate(List<string> codeList)
+    public static List<double?> Calculate(List<string> codeList, Dictionary<string, ParamValue> valueDic)
     {
         List<double?> result = new List<double?>();
 
-        foreach ((string? code, var exp) in ParamExpressionDic)
+        Dictionary<string, object> internalValueDic = new Dictionary<string, object>();
+        foreach ((string? key, var value) in valueDic)
         {
-            exp.Evaluate();
+            internalValueDic.Add(key, value.NumberMode ? value.NumberValue : value.StringValue);
         }
 
         foreach ((int _, var list) in FormulaLevelDic)
@@ -232,9 +177,27 @@ public class PveUtility
             {
                 try
                 {
-                    if (!SpecialFormulaRuleDic.ContainsKey(code) && InternalFormulaExpressionDic.TryGetValue(code, out var codeExp))
+                    if (InternalFormulaExpressionDic.TryGetValue(code, out var codeExp))
                     {
-                        codeExp.Evaluate();
+                        if (!SpecialFormulaRuleDic.ContainsKey(code))
+                        {
+                            foreach (string? paramCode in codeExp.Parameters.Keys)
+                            {
+                                codeExp.Parameters[paramCode] = internalValueDic[paramCode];
+                            }
+
+                            internalValueDic.Add(code, codeExp.Evaluate());
+                        }
+                        else
+                        {
+                            foreach ((string? paramCode, object? _) in codeExp.Parameters)
+                            {
+                                if (internalValueDic.TryGetValue(paramCode, out object? paramValue))
+                                {
+                                    codeExp.Parameters[paramCode] = paramValue;
+                                }
+                            }
+                        }
                     }
                 }
                 catch (Exception)
@@ -244,23 +207,19 @@ public class PveUtility
                 }
             }
         }
-
+        
         foreach (string code in codeList)
         {
             if (ResultFormulaExpressionDic.TryGetValue(code, out var formulaExp))
             {
                 foreach ((string? key, object? _) in formulaExp.Parameters)
                 {
-                    if (ParamExpressionDic.TryGetValue(key, out var paramExp))
+                    if (internalValueDic.TryGetValue(key, out object? internalValue))
                     {
-                        formulaExp.Parameters[key] = paramExp.Evaluate();
-                    }
-                    else if (InternalFormulaExpressionDic.TryGetValue(key, out var subFormulaExp))
-                    {
-                        formulaExp.Parameters[key] = subFormulaExp;
+                        formulaExp.Parameters[key] = internalValue;
                     }
                 }
-
+        
                 try
                 {
                     if (double.TryParse(formulaExp.Evaluate().ToString(), out double value))
