fd1fb8f4b86203ab744120d500eb85d514ebeac4
dellbeat <daizhicai@foxmail.com>
2025-01-13 00:19:23 +0800

    更新数据，增加紧凑模式，提升版本号（暂不发版）

--- NshmCalculator.MudClient/Utilities/ConstText.cs
+++ NshmCalculator.MudClient/Utilities/ConstText.cs
@@ -14,11 +14,13 @@ public static class ConstText
 
     public const string ConfigNotFoundText = "<ul><li>获取计算器配置时出现异常，请检查网络</li></ul>";
     
-    public const string FooterText = "Version:2.0.0 | Powered by .NET 8.0";
+    public const string FooterText = "Version:2.0.1 | Powered by .NET 8.0";
 
     public const string LastVisitName = "lastVisit";//最近一次访问的计算器路由
 
     public const string AutoRedirectName = "autoRedirect";//是否重定向
+
+    public const string DenseModeName = "denseMode";
     
     public const string VersionPath = "../data/version.json";
 
---
+++ NshmCalculator.Test/CalculatorUtility/IndexTest.cs
@@ -0,0 +1,15 @@
+﻿namespace NshmCalculator.Test.CalculatorUtility;
+
+[TestFixture]
+public class IndexTest
+{
+    private const string IndexPath = "data/index.html";
+
+    [Test]
+    public void IndexPageCheck()
+    {
+        string html = File.ReadAllText(IndexPath);
+        Assert.That(html.Contains("<!--    <script defer src=\"js/visitor.js\""), Is.False, "未反注释匿名访客统计代码");
+        Assert.Pass("统计代码校验通过");
+    }
+}
